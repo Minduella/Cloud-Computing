4f94b221c8e213880b50b395538e23ac867d6e58
Alcadeius <125739415+Alcadeius@users.noreply.github.com>
2024-12-02 21:43:39 +0800

    Delete routes directory

--- routes/entry.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { addentry, getallentry } = require("../controllers/entrycontroller");
-router.post("/", addentry);
-router.get("/", getallentry);
-
-module.exports = router;
